0e30e6632bfb66adc50f59c318e871019788b47d
EavOutDom <eavoutdom400@gmail.com>
2022-12-27 10:12:49 +0700

    hue-rotate

---
+++ pages/css/hue-rotate.js
@@ -0,0 +1,58 @@
+import { Button, Card, Slider } from "antd";
+import ContentLayout from "../../components/contentlayout/ContentLayout";
+import { useRef, useState } from "react";
+import { FaCopy } from "react-icons/fa";
+import { useCopy } from "../../lib/useCopy";
+
+const HueRotate = () => {
+  const [angle, setAngle] = useState(90);
+  const [copy, setCopy] = useCopy();
+  const ref = useRef();
+  return (<section>
+    <ContentLayout name='Hue-Rotate' back="/css" >
+      <ContentLayout.Paragraph>
+        <p>
+          {`The hue-rotate() CSS function rotates the hue of an element and its contents. Its result is a <filter-function>.`}
+        </p>
+      </ContentLayout.Paragraph>
+      <ContentLayout.Options>
+        <div>
+          <p className="content_title">Option</p>
+          <Card>
+            <label htmlFor="angle">Angle {angle}deg</label>
+            <Slider value={angle} onChange={setAngle} id='angle' max={360} />
+          </Card>
+        </div>
+      </ContentLayout.Options>
+      <ContentLayout.Preview>
+        <div>
+          <p className="content_title">Preview</p>
+          <Card>
+            <div
+              style={{
+                height: 200,
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+                backgroundImage: 'url(https://interactive-examples.mdn.mozilla.net/media/examples/firefox-logo.svg)',
+                filter: `hue-rotate(${angle}deg)`
+              }}
+            />
+          </Card>
+        </div>
+        <div>
+          <p className="content_title">Code</p>
+          <Card>
+            <div className="justify-between items-center">
+              <code ref={ref}>
+                filter: hue-rotate({angle}deg);
+              </code>
+              <Button onClick={() => setCopy(ref)} icon={<FaCopy />} />
+            </div>
+          </Card>
+        </div>
+      </ContentLayout.Preview>
+    </ContentLayout>
+  </section>);
+}
+
+export default HueRotate;
