170be78a929ab3bba09d9e3ceb9dbc379addf55e
EavOutDom <eavoutdom400@gmail.com>
2022-12-27 10:17:51 +0700

    invert

---
+++ pages/css/invert.js
@@ -0,0 +1,58 @@
+import { Button, Card, Slider } from "antd";
+import ContentLayout from "../../components/contentlayout/ContentLayout";
+import { useRef, useState } from "react";
+import { FaCopy } from "react-icons/fa";
+import { useCopy } from "../../lib/useCopy";
+
+const Invert = () => {
+  const [amount, setAmount] = useState(75);
+  const [copy, setCopy] = useCopy();
+  const ref = useRef();
+  return (<section>
+    <ContentLayout name='Invert' back="/css" >
+      <ContentLayout.Paragraph>
+        <p>
+          {`The invert() CSS function inverts the color samples in the input image. Its result is a <filter-function>.`}
+        </p>
+      </ContentLayout.Paragraph>
+      <ContentLayout.Options>
+        <div>
+          <p className="content_title">Option</p>
+          <Card>
+            <label htmlFor="invert">Invert {amount}%</label>
+            <Slider value={amount} onChange={setAmount} id='invert' />
+          </Card>
+        </div>
+      </ContentLayout.Options>
+      <ContentLayout.Preview>
+        <div>
+          <p className="content_title">Preview</p>
+          <Card>
+            <div
+              style={{
+                height: 200,
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+                backgroundImage: 'url(https://interactive-examples.mdn.mozilla.net/media/examples/firefox-logo.svg)',
+                filter: `invert(${amount}%)`
+              }}
+            />
+          </Card>
+        </div>
+        <div>
+          <p className="content_title">Code</p>
+          <Card>
+            <div className="justify-between items-center">
+              <code ref={ref}>
+                filter: invert({amount}%);
+              </code>
+              <Button onClick={() => setCopy(ref)} icon={<FaCopy />} />
+            </div>
+          </Card>
+        </div>
+      </ContentLayout.Preview>
+    </ContentLayout>
+  </section>);
+}
+
+export default Invert;
