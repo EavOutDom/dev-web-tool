c8d57a6e87a0d38d4494f858fae5b73cf9031ffc
EavOutDom <eavoutdom400@gmail.com>
2022-12-27 10:05:58 +0700

    grayscale

---
+++ pages/css/grayscale.js
@@ -0,0 +1,58 @@
+import { Button, Card, Slider } from "antd";
+import ContentLayout from "../../components/contentlayout/ContentLayout";
+import { useRef, useState } from "react";
+import { FaCopy } from "react-icons/fa";
+import { useCopy } from "../../lib/useCopy";
+
+const Grayscale = () => {
+  const [amount, setAmount] = useState(50);
+  const [copy, setCopy] = useCopy();
+  const ref = useRef();
+  return (<section>
+    <ContentLayout name='Grayscale' back="/css" >
+      <ContentLayout.Paragraph>
+        <p>
+          {`The grayscale() CSS function converts the input image to grayscale. Its result is a <filter-function>.`}
+        </p>
+      </ContentLayout.Paragraph>
+      <ContentLayout.Options>
+        <div>
+          <p className="content_title">Option</p>
+          <Card>
+            <label htmlFor="grayscale">Grayscale {amount}%</label>
+            <Slider value={amount} onChange={setAmount} id='grayscale' />
+          </Card>
+        </div>
+      </ContentLayout.Options>
+      <ContentLayout.Preview>
+        <div>
+          <p className="content_title">Preview</p>
+          <Card>
+            <div
+              style={{
+                height: 200,
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+                backgroundImage: 'url(https://interactive-examples.mdn.mozilla.net/media/examples/firefox-logo.svg)',
+                filter: `grayscale(${amount}%)`
+              }}
+            />
+          </Card>
+        </div>
+        <div>
+          <p className="content_title">Code</p>
+          <Card>
+            <div className="justify-between items-center">
+              <code ref={ref}>
+                filter: grayscale({amount}%);
+              </code>
+              <Button onClick={() => setCopy(ref)} icon={<FaCopy />} />
+            </div>
+          </Card>
+        </div>
+      </ContentLayout.Preview>
+    </ContentLayout>
+  </section>);
+}
+
+export default Grayscale;
